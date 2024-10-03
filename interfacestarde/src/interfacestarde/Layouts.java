87b38ad1e19fd953febfe73eb759aaccc70d615e
ferchuli08 <belmontef2006@gmail.com>
2024-10-03 15:51:32 +0200

    Add files via upload

---
+++ interfacestarde/src/interfacestarde/Layouts.java
@@ -0,0 +1,63 @@
+package interfacestarde;
+
+import java.awt.BorderLayout;
+import java.awt.EventQueue;
+
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.border.EmptyBorder;
+import javax.swing.JTextField;
+import javax.swing.JLabel;
+import javax.swing.JButton;
+import java.awt.GridLayout;
+
+public class Layouts extends JFrame {
+
+	private static final long serialVersionUID = 1L;
+	private JPanel contentPane;
+
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					Layouts frame = new Layouts();
+					frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
+	/**
+	 * Create the frame.
+	 */
+	public Layouts() {
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setBounds(100, 100, 450, 300);
+		contentPane = new JPanel();
+		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
+
+		setContentPane(contentPane);
+		contentPane.setLayout(new GridLayout(2, 3, 0, 0));
+		
+		JButton btnNewButton_4 = new JButton("New button");
+		contentPane.add(btnNewButton_4);
+		
+		JButton btnNewButton_3 = new JButton("New button");
+		contentPane.add(btnNewButton_3);
+		
+		JButton btnNewButton_2 = new JButton("New button");
+		contentPane.add(btnNewButton_2);
+		
+		JButton btnNewButton = new JButton("New button");
+		contentPane.add(btnNewButton);
+		
+		JButton btnNewButton_1 = new JButton("New button");
+		contentPane.add(btnNewButton_1);
+	}
+
+}
